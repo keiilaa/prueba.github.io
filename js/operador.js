eab07eb1d49747d77d6f0e4a7533efc6d1ef6d56
keiilaa <129077142+keiilaa@users.noreply.github.com>
2023-04-24 09:26:51 -0500

    main

--- js/operador.js
+++ js/operador.js
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
-<html>
-    
-</html>
+//Conversion larga: 
+var celsius1 = prompt("Introduzca la temperatura en grados celsius:");
+celsius1 = parseFloat(celsius1);
+
+//Conversion corta:
+//var celsius1 = parseFloat(prompt("Introduzca la temperatura en grados celsius: "));
+
+//Proceso a hacer la conversiomn de  celsius a Farenhit:
+var Farenhit = (9/5)*celsius1+32;
+
+document.write("De celsius a Farenheit es: ", Farenhit);
